4b269ad0e3ea42037b26a038387fff7d9c9376e6
Unknown <jsalinas_magallanes@hotmail.com>
2018-07-08 15:24:12 -0400

    status

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.listen(5000, function(){
+  console.log('Application running on port 5000!');
+});
 const Eureka = require('eureka-js-client').Eureka;
 
 const eureka = new Eureka({
@@ -55,7 +58,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
-
+app.get('/status', function (req, res){
+  res.send('ready');
+})
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
